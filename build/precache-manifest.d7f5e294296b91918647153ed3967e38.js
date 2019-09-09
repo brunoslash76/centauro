43a892bece68c02b6b72cc53ef5d82275f7e24d4
Bruno Tavares de Moraes Russo <bruno.russo@evolutionit.com>
2019-09-09 16:17:27 -0300

    first commit to heroku

---
+++ build/precache-manifest.d7f5e294296b91918647153ed3967e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e9fa9f440e8b44c3e068c485469966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e34a3090cecd643fdc56",
+    "url": "/static/css/main.f4c06474.chunk.css"
+  },
+  {
+    "revision": "19629f87886cb02815f7",
+    "url": "/static/js/2.6bb229b4.chunk.js"
+  },
+  {
+    "revision": "e34a3090cecd643fdc56",
+    "url": "/static/js/main.37a2eff7.chunk.js"
+  },
+  {
+    "revision": "138c43e65b2e2f1c93ad",
+    "url": "/static/js/runtime~main.55fb6f09.js"
+  },
+  {
+    "revision": "b011b0ee9ff258f60075bb5e5ba5d479",
+    "url": "/static/media/loading.b011b0ee.gif"
+  }
+]);
